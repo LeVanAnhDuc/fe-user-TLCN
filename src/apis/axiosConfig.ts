8824fe7ad1f8458fce468d441bee270f76c25d6a
LrVanAnhDuc <levananhduc1804@gmail.com>
2023-12-16 17:41:16 +0700

    update api production 2

--- src/apis/axiosConfig.ts
+++ src/apis/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'https://duckshop.onrender.com/api/v1',
-    timeout: 30000, // Thời gian timeout cho mỗi request
+    timeout: 300000, // Thời gian timeout cho mỗi request
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
