c7e062b7e7f210667bbf8c1663fd7477937acd58
LrVanAnhDuc <levananhduc1804@gmail.com>
2024-04-04 22:46:00 +0700

    refactor: replace utils  objectsAreEqual

--- src/utils/checkData.ts
+++ src/utils/checkData.ts
@@ -1,17 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const objectsAreEqual = (obj1: { [key: string]: any }, obj2: { [key: string]: any }): boolean => {
-    const keys1 = Object.keys(obj1);
-    const keys2 = Object.keys(obj2);
-
-    if (keys1.length !== keys2.length) {
+export const objectsAreEqual = (obj1: object, obj2: object): boolean => {
+    if (JSON.stringify(obj1) === JSON.stringify(obj2)) {
+        return true;
+    } else {
         return false;
     }
-
-    for (const key of keys1) {
-        if (obj1[key].trim() !== obj2[key].trim()) {
-            return false;
-        }
-    }
-
-    return true;
 };
