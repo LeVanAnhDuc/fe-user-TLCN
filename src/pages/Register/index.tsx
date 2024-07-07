f9c4e042d754ae2e07c25951c586e59024fa3de6
LrVanAnhDuc <levananhduc1804@gmail.com>
2024-07-07 22:38:10 +0700

    refactor code register

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,83 +1,21 @@
-import TextField from '@mui/material/TextField';
-
-import { useForm, SubmitHandler, Controller } from 'react-hook-form';
-import { Link, useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
+// libs
+import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-
-import config from '../../config';
-import { useAppDispatch } from '../../redux/hook';
-import { setRegister } from './registerSlice';
-import { registerApi } from '../../apis/authApi';
-import AnimationScale from '../../components/AnimationScale';
-import AnimationTran from '../../components/AnimationTran';
-import Button from '../../components/Button';
-import SnackBarLoading from '../../components/SnackBarLoading';
-import Logo from '../../components/Logo';
-import InputPassword from '../../components/InputPassword';
-
-type FormDataResgister = {
-    email: string;
-    userName: string;
-    passWord: string;
-};
+// components
+import AnimationScale from '@/components/AnimationScale';
+import AnimationTran from '@/components/AnimationTran';
+import Button from '@/components/Button';
+import SnackBarLoading from '@/components/SnackBarLoading';
+import Logo from '@/components/Logo';
+import Form from './mains/Form';
+// others
+import config from '@/config';
 
 const Register = () => {
-    const dispatch = useAppDispatch();
-    const navigate = useNavigate();
     const { t } = useTranslation('register');
 
-    const [isLoadingRegister, setIsLoadingRegister] = useState(false);
-
-    const schema = yup.object().shape({
-        email: yup.string().required(t('emailIsRequired')).email(t('emailIsValidate')),
-        userName: yup.string().required(t('usernameIsRequired')).min(4, t('UsernameLeast4')),
-        passWord: yup
-            .string()
-            .required(t('passwordIsRequired'))
-            .test('is-passWord', t('passwordIsNotFormat'), function (value) {
-                if (!value) return true;
-
-                const regexUserName = /^[a-zA-Z0-9]+$/;
-
-                return regexUserName.test(value);
-            })
-            .min(8, t('passwordLeast8')),
-    });
-
-    const {
-        control,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<FormDataResgister>({
-        resolver: yupResolver(schema),
-    });
-
-    const onSubmit: SubmitHandler<FormDataResgister> = async (data: FormDataResgister) => {
-        try {
-            setIsLoadingRegister(true);
-            const response = await registerApi(data.userName, data.email, data.passWord);
-            setIsLoadingRegister(false);
-
-            if (response.status === 201) {
-                dispatch(
-                    setRegister({
-                        email: data.email,
-                        passWord: data.passWord,
-                    }),
-                );
-                toast.success(t('registerSuccessful'));
-                navigate(config.Routes.getOTPRegister);
-            } else {
-                toast.error(t('registerFailed'));
-            }
-        } catch (error) {
-            toast.error(t('registerFailed'));
-        }
-    };
+    const [isLoadingRegister, setIsLoadingRegister] = useState<boolean>(false);
 
     return (
         <>
@@ -98,70 +36,7 @@ const Register = () => {
                             {t('register')}
                         </AnimationTran>
 
-                        <form className="space-y-3" onSubmit={handleSubmit(onSubmit)}>
-                            <AnimationTran tranX={-100} delay={0.1}>
-                                <>
-                                    <Controller
-                                        name="email"
-                                        control={control}
-                                        render={({ field }) => (
-                                            <TextField
-                                                {...field}
-                                                error={errors.email ? true : false}
-                                                fullWidth
-                                                label={t('enterEmail')}
-                                                autoComplete="email"
-                                            />
-                                        )}
-                                    />
-                                    <p className="text-red-600 text-sm py-1 h-6 dark:text-red-500">
-                                        {errors.email?.message}
-                                    </p>
-                                </>
-                            </AnimationTran>
-                            <AnimationTran tranX={-100} delay={0.2}>
-                                <>
-                                    <Controller
-                                        name="userName"
-                                        control={control}
-                                        render={({ field }) => (
-                                            <TextField
-                                                {...field}
-                                                error={errors.userName ? true : false}
-                                                fullWidth
-                                                label={t('enterUsername')}
-                                            />
-                                        )}
-                                    />
-                                    <p className="text-red-600 text-sm py-1 h-6 dark:text-red-500">
-                                        {errors.userName?.message}
-                                    </p>
-                                </>
-                            </AnimationTran>
-                            <AnimationTran tranX={-100} delay={0.3}>
-                                <>
-                                    <Controller
-                                        name="passWord"
-                                        control={control}
-                                        render={({ field }) => (
-                                            <InputPassword
-                                                field={{ ...field }}
-                                                error={errors.passWord ? true : false}
-                                                label={t('enterPassword')}
-                                            />
-                                        )}
-                                    />
-                                    <p className="text-red-600 text-sm py-1 h-6 dark:text-red-500">
-                                        {errors.passWord?.message}
-                                    </p>
-                                </>
-                            </AnimationTran>
-                            <AnimationTran tranX={-100} delay={0.4}>
-                                <Button type="submit" fullWidth variant="fill" loading={isLoadingRegister}>
-                                    {t('register')}
-                                </Button>
-                            </AnimationTran>
-                        </form>
+                        <Form {...{ setIsLoadingRegister, isLoadingRegister }} />
 
                         <AnimationTran tranY={100} delay={0.5} className="text-center text-sm ">
                             <div className="flex place-content-center place-items-center">
